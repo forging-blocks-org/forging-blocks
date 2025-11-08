edb64c1e071c489a21494d8f48202b7a582da9be
Glauber Brennon <glauberbrennon@gmail.com>
2025-11-08 07:02:29 -0300

    Wrote docstrings for BuildingBlocks __init__ files.

--- src/building_blocks/domain/messages/__init__.py
+++ src/building_blocks/domain/messages/__init__.py
@@ -1,11 +1,8 @@
-"""Init module.
+"""Domain messages package."""
 
-Auto-generated minimal module docstring.
-"""
-
-from building_blocks.domain.messages.command import Command
-from building_blocks.domain.messages.event import Event
-from building_blocks.domain.messages.message import Message, MessageMetadata
-from building_blocks.domain.messages.query import Query
+from .command import Command
+from .event import Event
+from .message import Message, MessageMetadata
+from .query import Query
 
 __all__ = ["Message", "MessageMetadata", "Event", "Command", "Query"]
