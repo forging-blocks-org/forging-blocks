7580ae9ed558e0cefd9e1be49d9465a926121544
Glauber Brennon <glauberbrennon@gmail.com>
2025-12-13 22:02:09 -0300

    Domain import shortcuts

--- src/forging_blocks/domain/__init__.py
+++ src/forging_blocks/domain/__init__.py
@@ -1,1 +1,21 @@
 """ForgingBlocks for domain-specific modules."""
+
+from .aggregate_root import AggregateRoot, AggregateVersion
+from .entity import Entity
+from .errors import DraftEntityIsNotHashableError, EntityIdNoneError
+from .messages import Command, Event, Message, MessageMetadata, Query
+from .value_object import ValueObject
+
+__all__ = [
+    "DraftEntityIsNotHashableError",
+    "Command",
+    "EntityIdNoneError",
+    "Entity",
+    "Event",
+    "Message",
+    "MessageMetadata",
+    "Query",
+    "AggregateRoot",
+    "AggregateVersion",
+    "ValueObject",
+]
--- src/forging_blocks/domain/errors/__init__.py
+++ src/forging_blocks/domain/errors/__init__.py
@@ -1,1 +1,9 @@
 """ForgingBlocks for domain errors."""
+
+from .draft_entity_is_not_hashable_error import DraftEntityIsNotHashableError
+from .entity_id_none_error import EntityIdNoneError
+
+__all__ = [
+    "DraftEntityIsNotHashableError",
+    "EntityIdNoneError",
+]
