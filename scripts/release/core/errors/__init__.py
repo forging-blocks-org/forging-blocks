35e664a7c8ccaf9d34ad8ec5298c409e14b0faf8
Glauber Brennon <glauberbrennon@gmail.com>
2025-12-18 21:42:49 -0300

    Implemented core errors

---
+++ scripts/release/core/errors/__init__.py
@@ -0,0 +1,15 @@
+from .invalid_pull_request_title_error import InvalidPullRequestTitleError
+from .invalid_release_version_error import InvalidReleaseVersionError
+from .invalid_release_level_error import InvalidReleaseLevelError
+from .invalid_release_branch_name_error import InvalidReleaseBranchNameError
+from .invalid_tag_name_error import InvalidTagNameError
+from .invalid_release_branch_error import InvalidReleaseBranchNameError
+
+__all__ = [
+    "InvalidReleaseBranchNameError",
+    "InvalidTagNameError",
+    "InvalidReleaseLevelError",
+    "InvalidTagNameError",
+    "InvalidReleaseVersionError",
+    "InvalidPullRequestTitleError",
+]
