89dab95ecf7bcbb51647d3beacc5c39656f2e4dc
Glauber Brennon <glauberbrennon@gmail.com>
2026-01-27 23:30:23 -0300

    Exporting ReleaseBranchExistsError in `strings/release/application/errors/__init__.py`

--- scripts/release/application/errors/__init__.py
+++ scripts/release/application/errors/__init__.py
@@ -1,5 +1,7 @@
+from .release_branch_exists_error import ReleaseBranchExistsError
 from .tag_already_exists_error import TagAlreadyExistsError
 
 __all__ = [
     "TagAlreadyExistsError",
+    "ReleaseBranchExistsError",
 ]
