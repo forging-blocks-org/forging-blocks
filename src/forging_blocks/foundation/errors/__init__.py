c7a305bfa0c2db4ce9baccbaf869d0c6dcda5d4f
Glauber Brennon <glauberbrennon@gmail.com>
2026-06-03 03:13:12 -0300

    refactor(foundation): re-export module initializer

--- src/forging_blocks/foundation/errors/__init__.py
+++ src/forging_blocks/foundation/errors/__init__.py
@@ -3,6 +3,7 @@
 from .base import CombinedErrors, Error, FieldErrors, NoneNotAllowedError
 from .cant_modify_immutable_attribute_error import CantModifyImmutableAttributeError
 from .core import ErrorMessage, ErrorMetadata, FieldReference
+from .result_access_error import ResultAccessError
 from .rule_violation_error import CombinedRuleViolationErrors, RuleViolationError
 from .validation_error import (
     CombinedValidationErrors,
@@ -20,6 +21,7 @@ __all__ = [
     "ErrorMessage",
     "ErrorMetadata",
     "FieldReference",
+    "ResultAccessError",
     "RuleViolationError",
     "ValidationError",
     "ValidationFieldErrors",
