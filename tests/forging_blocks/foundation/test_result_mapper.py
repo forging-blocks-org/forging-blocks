b1592636ce7833e9d535966c69fef6247a2e5b17
Glauber Brennon <glauberbrennon@gmail.com>
2026-06-07 22:10:04 -0300

    feat(foundation)!: remove ResultMapper

--- tests/forging_blocks/foundation/test_result_mapper.py
+++
@@ -1,1 +0,0 @@
-# pyright: reportPrivateUsage=false, reportMissingTypeArgument=false, reportUnknownParameterType=false, reportUnknownMemberType=false, reportUnknownVariableType=false, reportUnknownArgumentType=false, reportMissingParameterType=false, reportIncompatibleMethodOverride=false, reportUnusedClass=false, reportFunctionMemberAccess=false
