672d17af7a530e85604c7df77909c67ab4d5f429
Glauber Brennon <glauberbrennon@gmail.com>
2026-07-03 03:54:10 -0300

    feat(application): add TransactionError

--- src/forging_blocks/application/errors/__init__.py
+++ src/forging_blocks/application/errors/__init__.py
@@ -1,11 +1,13 @@
 from .concurrency_error import ConcurrencyError
 from .event_bus_error import EventBusError
 from .event_store_error import EventStoreError
+from .transaction_error import TransactionError
 from .unit_of_work_error import UnitOfWorkError
 
 __all__ = [
     "ConcurrencyError",
     "EventBusError",
     "EventStoreError",
+    "TransactionError",
     "UnitOfWorkError",
 ]
