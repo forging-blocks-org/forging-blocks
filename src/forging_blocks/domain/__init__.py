a32315a4ecf904fd58d25ae42a183d63791f03f7
Glauber Brennon <glauberbrennon@gmail.com>
2026-07-03 03:53:44 -0300

    feat(domain): add CompositePermissionChecker and re-export from domain

--- src/forging_blocks/domain/__init__.py
+++ src/forging_blocks/domain/__init__.py
@@ -16,20 +16,38 @@ from .errors import (
     EntityIdModificationError,
     EntityIdNoneError,
 )
+from .permissions.composite_permission_checker import CompositePermissionChecker
+from .permissions.permission_checker import PermissionChecker
+from .permissions.resource_permission_checker import ResourcePermissionChecker
+from .permissions.role_based_permission_checker import RoleBasedPermissionChecker
+from .validators.composite_validation_rule import CompositeValidationRule
+from .validators.email_validator import EmailValidator
+from .validators.length_validator import LengthValidator
+from .validators.range_validator import RangeValidator
+from .validators.required_validator import RequiredValidator
 from .value_object import ValueObject
 
 __all__ = [
+    "AggregateRoot",
+    "AggregateVersion",
+    "AndSpecification",
+    "CompositePermissionChecker",
+    "CompositeValidationRule",
     "DraftEntityIsNotHashableError",
+    "EmailValidator",
+    "Entity",
     "EntityIdDeletionError",
     "EntityIdModificationError",
     "EntityIdNoneError",
-    "Entity",
-    "AggregateRoot",
-    "AggregateVersion",
-    "ValueObject",
-    "Specification",
     "ExpressionSpecification",
-    "AndSpecification",
-    "OrSpecification",
+    "LengthValidator",
     "NotSpecification",
+    "OrSpecification",
+    "PermissionChecker",
+    "RangeValidator",
+    "RequiredValidator",
+    "ResourcePermissionChecker",
+    "RoleBasedPermissionChecker",
+    "Specification",
+    "ValueObject",
 ]
