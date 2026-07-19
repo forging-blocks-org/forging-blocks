ac31ca7ddbeddc86c63649d618617d1a526676d4
Glauber Brennon <glauberbrennon@gmail.com>
2026-07-19 00:19:01 -0300

    feat(presentation): register ValidationMiddleware and ErrorHandlingMiddleware in builtin exports

--- src/forging_blocks/presentation/builtin/__init__.py
+++ src/forging_blocks/presentation/builtin/__init__.py
@@ -4,10 +4,14 @@ These are concrete, reusable middleware classes that compose with
 ``Pipeline`` to add cross-cutting behaviour around use-case execution.
 """
 
+from .error_handling_middleware import ErrorHandlingMiddleware
 from .logging_middleware import LoggingMiddleware
 from .timing_middleware import TimingMiddleware
+from .validation_middleware import ValidationMiddleware
 
 __all__ = [
+    "ErrorHandlingMiddleware",
     "LoggingMiddleware",
     "TimingMiddleware",
+    "ValidationMiddleware",
 ]
